f37158543502104e183943e86e918d8ea81c9b26
Iman Ragab <iman.mansour94@gmail.com>
2018-08-02 00:08:19 +0200

    add login design

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material'
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
@@ -20,6 +33,18 @@ import { FieldErrorDisplayComponent } from './field-error-display/field-error-di
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatIconModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatProgressSpinnerModule,
     BrowserAnimationsModule
   ],
   providers: [],
